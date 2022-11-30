de8152fb58c7770ff1296591f1e73f755781a989
Daniel Gustaw <gustaw.daniel@gmail.com>
2022-11-30 10:26:08 +0400

    added backup base url address

--- front_nuxt/nuxt.config.ts
+++ front_nuxt/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
     ssr: false,
     runtimeConfig: {
         public: {
-            baseUrl: process.env.NUXT_PUBLIC_BASE_URL,
+            baseUrl: process.env.NUXT_PUBLIC_BASE_URL ?? 'https://strapi.admeal.xyz',
         },
     },
 })
